763e479808e3fbf1153fe81bef000997298ebc08
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2024-03-10 07:17:13 +0100

    Update PCH.h

--- src/PCH.h
+++ src/PCH.h
@@ -3,24 +3,4 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-
-
-#include <shared_mutex>
-
-#include "RE/Skyrim.h"
-#include "SKSE/SKSE.h"
-
-#ifdef NDEBUG
-#	include <spdlog/sinks/basic_file_sink.h>
-#else
-#	include <spdlog/sinks/msvc_sink.h>
-#endif
-#include <xbyak/xbyak.h>
-#include <srell.hpp>
-
-#include "ClibUtil/editorID.hpp"
-#include "ClibUtil/simpleINI.hpp"
-#include "ClibUtil/singleton.hpp"
-#include "ClibUtil/string.hpp"
-
 using namespace std::literals;
