32cb23f92ddad83ca7244c38ee1fbe00fc6c119d
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2024-03-11 02:32:58 +0100

    Update logger.h

--- src/logger.h
+++ src/logger.h
@@ -12,10 +12,11 @@ void SetupLog() {
     auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
     auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
     spdlog::set_default_logger(std::move(loggerPtr));
-    spdlog::set_level(spdlog::level::trace);
 #ifndef NDEBUG
+    spdlog::set_level(spdlog::level::trace);
     spdlog::flush_on(spdlog::level::trace);
 #else
+    spdlog::set_level(spdlog::level::info);
     spdlog::flush_on(spdlog::level::info);
 #endif
     logger::info("Name of the plugin is {}.", pluginName);
